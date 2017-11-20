739e2b733bf06c89c007ffb5fcb74045665da0e8
Denis Egorychev <den.egorychev@yandex.ru>
2017-11-20 23:01:54 +0400

    Все сломал, зато Material-UI

---
+++ src/bar.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import { List, ListItem } from 'material-ui/List';
+import { IndexLink, Link, browserHistory, BrowserRouter } from 'react-router-dom';
+
+/**
+ * A simple example of `AppBar` with an icon on the right.
+ * By default, the left icon is a navigation-menu.
+ */
+
+export default class Bar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          open: false
+        };
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+
+    handleToggle() {
+        this.setState({ open: !this.state.open });
+    }
+
+    render() {
+        return (
+            <div>
+                <AppBar
+                onLeftIconButtonTouchTap={this.handleToggle}
+                title="Расписание ИАТУ"
+                />
+                <Drawer
+                docked={false}
+                width={200}
+                open={this.state.open}
+                onRequestChange={(open) => this.setState({ open })}
+                >
+                    <BrowserRouter>
+                        <List>
+                            <ListItem
+                            onClick={this.handleToggle}
+                            primaryText="Главная"
+                            containerElement={<Link to="/" />}
+                           />
+                            <ListItem
+                            onClick={this.handleToggle}
+                            primaryText="О приложении"
+                            containerElement={<Link to="/about" />}
+                            />
+                        </List>
+                    </BrowserRouter>
+                </Drawer>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
+import Bar from './bar';
 
 import './index.css';
 
@@ -42,118 +41,132 @@ function getGroup(url){
   xhttp.send();
 }
 
-class Week extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h3>Расписание</h3>
-        <hr />
-        <List data={group} />
-        <hr />
-        <h4>Понедельник</h4><OneDay data={shed.Monday} /><hr />
-        <h4>Вторник</h4><OneDay data={shed.Tuesday} /><hr />
-        <h4>Среда</h4><OneDay data={shed.Wednesday} /><hr />
-        <h4>Четверг</h4><OneDay data={shed.Thursday} /><hr />
-        <h4>Пятница</h4><OneDay data={shed.Friday} /><hr />
-        <h4>Суббота</h4><OneDay data={shed.Saturday} /><hr />
-      </div>
-    );
-  }
-}
 
-class List extends Component {  
-  state = {
-    value: 15,
-  };
 
-  handleChange = (event, index, value) => this.setState({value});
 
-  render() {
-    var data = this.props.data;
-    var ListTemplate;
-
-    if (data.length > 0) {
-      ListTemplate = data.map(function(item) {
-        return (
-          <MenuItem value={(item.ID-0)} primaryText={item.Naimenovanie} />
-        )
-      })
-    } else {
-      ListTemplate = <MenuItem value={1} primaryText="Ошибка" />
-    }
-
+class App extends Component {
+  render(){
     return(
-        <SelectField
-          floatingLabelText="Группа"
-          value={this.state.value}
-          onChange={this.handleChange}
-        >
-        {ListTemplate}
-        </SelectField>
-    )
+      <Bar />
+    );  
   }
 }
 
-class OneDay extends Component {
-  render() {
-    var data = this.props.data;
-    var PairsTemplate;
 
-    if (data.length > 0) {
-      PairsTemplate = data.map(function(item, index) {
-        return (
-          <div key={index}>
-            <Pair data={item} />
-          </div>
-        )
-      })
-    } else {
-      PairsTemplate = <p>Пар в этот день нет</p>
-    }
-    
-    return (
-      <div className='news'>
-        {PairsTemplate}
-        <strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего пар: {data.length}</strong>
-      </div>
-    );
-  }
-}
 
-// class Comments extends Component {
+
+// class Week extends Component {
 //   render() {
 //     return (
-//       <div className="comments">
-//         Нет новостей - комментировать нечего
+//       <div className='app'>
+//         <h3>Расписание</h3>
+//         <hr />
+//         <List data={group} />
+//         <hr />
+//         <h4>Понедельник</h4><OneDay data={shed.Monday} /><hr />
+//         <h4>Вторник</h4><OneDay data={shed.Tuesday} /><hr />
+//         <h4>Среда</h4><OneDay data={shed.Wednesday} /><hr />
+//         <h4>Четверг</h4><OneDay data={shed.Thursday} /><hr />
+//         <h4>Пятница</h4><OneDay data={shed.Friday} /><hr />
+//         <h4>Суббота</h4><OneDay data={shed.Saturday} /><hr />
 //       </div>
 //     );
 //   }
 // }
 
-class Pair extends Component {
-  render() {
-    var author = this.props.data.disc,
-    grup = this.props.data.group,
-    kab = this.props.data.kabinet,
-    tip = this.props.data.type,
-    tstart = this.props.data.start;
+// class List extends Component {  
+//   state = {
+//     value: 15,
+//   };
 
-    return (
-      <div className='article'>
-        <p className='news__author'>{author}:</p>
-        <p className='news__text'>{tstart}</p>
-        <p className='news__text'>{grup}</p>
-        <p className='news__text'>{kab}</p>
-        <p className='news__text'>{tip}</p>
-      </div>
-    );
-  }
-}
+//   handleChange = (event, index, value) => this.setState({value});
+
+//   render() {
+//     var data = this.props.data;
+//     var ListTemplate;
+
+//     if (data.length > 0) {
+//       ListTemplate = data.map(function(item) {
+//         return (
+//           <MenuItem value={(item.ID-0)} primaryText={item.Naimenovanie} />
+//         )
+//       })
+//     } else {
+//       ListTemplate = <MenuItem value={1} primaryText="Ошибка" />
+//     }
+
+//     return(
+//         <SelectField
+//           floatingLabelText="Группа"
+//           value={this.state.value}
+//           onChange={this.handleChange}
+//         >
+//         {ListTemplate}
+//         </SelectField>
+//     )
+//   }
+// }
+
+// class OneDay extends Component {
+//   render() {
+//     var data = this.props.data;
+//     var PairsTemplate;
+
+//     if (data.length > 0) {
+//       PairsTemplate = data.map(function(item, index) {
+//         return (
+//           <div key={index}>
+//             <Pair data={item} />
+//           </div>
+//         )
+//       })
+//     } else {
+//       PairsTemplate = <p>Пар в этот день нет</p>
+//     }
+    
+//     return (
+//       <div className='news'>
+//         {PairsTemplate}
+//         <strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего пар: {data.length}</strong>
+//       </div>
+//     );
+//   }
+// }
+
+// // class Comments extends Component {
+// //   render() {
+// //     return (
+// //       <div className="comments">
+// //         Нет новостей - комментировать нечего
+// //       </div>
+// //     );
+// //   }
+// // }
+
+// class Pair extends Component {
+//   render() {
+//     var author = this.props.data.disc,
+//     grup = this.props.data.group,
+//     kab = this.props.data.kabinet,
+//     tip = this.props.data.type,
+//     tstart = this.props.data.start;
+
+//     return (
+//       <div className='article'>
+//         <p className='news__author'>{author}:</p>
+//         <p className='news__text'>{tstart}</p>
+//         <p className='news__text'>{grup}</p>
+//         <p className='news__text'>{kab}</p>
+//         <p className='news__text'>{tip}</p>
+//       </div>
+//     );
+//   }
+// }
 
 
 ReactDOM.render(
   <MuiThemeProvider>
-  <Week />
+  <App />
   </MuiThemeProvider>,
   document.getElementById('root')
 );
